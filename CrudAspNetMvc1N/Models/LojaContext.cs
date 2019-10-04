93ff2be618dcbfde0ae44a3d3d03b13140f4e5a2
Ajala <Ajala@DESKTOP-AIP4D1G>
2019-10-04 16:19:58 -0400

    Criação da Classe LojaContext E cria String de conexão no Web.Config

---
+++ CrudAspNetMvc1N/Models/LojaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace CrudAspNetMvc1N.Models
+{
+    public class LojaContext : DbContext
+    {
+        //Aponta para a String de Conexao
+        public LojaContext() : base("Loja") { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Consultor> Consultores { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+    }
+}
